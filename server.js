8f99aa4af42c2db8dbee0233842e108fad892699
[mamounebennis1] <[kaka1181@hotmail.com]>
2021-01-26 00:25:44 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/'));
 
-app.get('*', (req, res) => {
-  res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
